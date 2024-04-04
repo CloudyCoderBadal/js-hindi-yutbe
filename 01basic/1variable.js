e26fc1a2fe2edd4155801a564d0bbcf48dd38be4
CloudyCoderBadal <165754063+CloudyCoderBadal@users.noreply.github.com>
2024-04-04 05:23:33 +0000

    datatype

---
+++ 01basic/1variable.js
@@ -0,0 +1,16 @@
+const accountid =143344
+let accountEmail ="badaloberio@gamil.com"
+var accountpassword = "12345"
+accountCity = "jaipur"
+
+//accountid = 2
+accountEmail = "hats@gamil.accountEmail"
+accountpassword ="21212121"
+accountCity ="begalur"
+
+
+
+
+console.log( accountid);
+
+console.table ([ accountid,accountEmail,accountpassword,accountCity])
---
+++ 01basic/2- datatype.js
@@ -0,0 +1,24 @@
+"use strict"; //trat all js code as newer 
+
+//alert( 3+3) //we are using node js not browser//
+
+let name = " badal   "
+let age = 13 
+let IsLoggedIn = false
+
+
+//number => 2 to power 53
+// bignit ( trading )
+// string => "" 
+// boolean => true/ false
+//null => standalonevalue
+// symbol =>  unique
+
+
+
+//object
+
+
+
+console.log (  typeof undefined); //undefined
+console.log (  typeof null); // object
