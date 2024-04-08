2324c8a296b348859509b08b4e8b5a4572f5e08b
CloudyCoderBadal <165754063+CloudyCoderBadal@users.noreply.github.com>
2024-04-08 05:30:53 +0000

    converstion js

--- 01basic/3-conversionoperation.js
+++ 01basic/3-conversionoperation.js
@@ -13,7 +13,38 @@
 // "33abc" => nan
 // true 1; false => 0
 
-let isLoggedIn = 1
+/** let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean ( isLoggedIn)
  console.log (booleanIsLoggedIn);
+
+
+ let someNumber = 33
+
+ let stringNumber = string (someNumber)
+  
+ console.log ( stringNumber);
+ console.log( typeof stringnumber);**/
+
+
+
+ /////****************************operations*****************************//
+
+
+ /**let value = 3
+ let negvalue = -value 
+ console.log (negvalue) ;
+
+  console.log (2+2);
+  console.log (2/3);
+  console.log (2**3);
+  console.log (2%3);**/
+  
+let str1 = "hello"
+let str2 = "badal"
+
+let str3 = str1 + str2
+
+console.log ( str3);
+
+
